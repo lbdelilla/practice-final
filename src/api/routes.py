92bdfdfc69a4c5f3da27e66f1303325a81c113cb
Lucía Belén <lbdelilla@gmail.com>
2023-10-06 16:56:31 +0000

    fsdf

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,7 +5,7 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
-
+## comment 
 api = Blueprint('api', __name__)
 
 
